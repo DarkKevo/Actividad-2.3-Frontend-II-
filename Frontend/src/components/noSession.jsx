8bee0c884eea90e6e7b58dd46b8669392aa75368
victoriaQuintero <victoria170460@gmail.com>
2023-02-25 14:14:08 -0400

    responsive y estilos terminados

--- Frontend/src/components/noSession.jsx
+++ Frontend/src/components/noSession.jsx
@@ -1,8 +1,8 @@
-import "../styles/noSession.css"
+
 
 export function NoSession() {
   return (
-    <div className='container d-flex align-items-center justify-content-center'>
+    <div className='container d-flex align-items-center justify-content-center p-5 text-light'>
       <h2>No Tienes una sesion Iniciada</h2>
     </div>
   );
