4b42db2bb4954d7b6e7d33307b712668730fc563
victoriaQuintero <victoria170460@gmail.com>
2023-02-25 14:05:53 -0400

    responsive y estilos terminados

--- Frontend/src/components/modal.jsx
+++ Frontend/src/components/modal.jsx
@@ -1,12 +1,12 @@
-import { FaPlus } from 'react-icons/fa';
-import { useContext, useState, useEffect } from 'react';
-import { dataContext } from '../context/context';
+import { FaPlus } from "react-icons/fa";
+import { useContext, useState, useEffect } from "react";
+import { dataContext } from "../context/context";
 
 function Modal() {
-  let data = JSON.parse(localStorage.getItem('Sesion'));
-  const [descripcion, setDescripcion] = useState('');
-  const [categoria, setCategoria] = useState('all');
-  const [file, setFile] = useState('');
+  let data = JSON.parse(localStorage.getItem("Sesion"));
+  const [descripcion, setDescripcion] = useState("");
+  const [categoria, setCategoria] = useState("all");
+  const [file, setFile] = useState("");
 
   const { CargarImagen } = useContext(dataContext);
 
@@ -14,72 +14,84 @@ function Modal() {
     e.preventDefault();
     console.log(descripcion, categoria, file);
     CargarImagen(descripcion, categoria, data.id, file, data.token);
-    setFile('');
-    setDescripcion('');
-    setCategoria('all');
+    setFile("");
+    setDescripcion("");
+    setCategoria("all");
   };
 
   return (
     <div>
       {/* <!-- Button trigger modal --> */}
       <button
-        type='button'
-        className='btn bg-secondary rounded-5 m-5 p-3 position-fixed bottom-0 end-0 z-2'
-        data-bs-toggle='modal'
-        data-bs-target='#exampleModal'
+        type="button"
+        className="btn bg-secondary rounded-5 m-5 p-3 position-fixed bottom-0 end-0 z-2"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
       >
-        <FaPlus className='fs-2' />
+        <FaPlus className="fs-2" />
       </button>
 
       {/* <!-- Modal --> */}
-      <div className='modal fade' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-        <form onSubmit={ModalHandler}>
-          <div className='modal-dialog modal-fullscreen-sm-down'>
-            <div className='modal-content bg-black'>
-              <div className='modal-header'>
-                <h1 className='modal-title fs-5' id='exampleModalLabel'>
-                  Modal title
-                </h1>
-                <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-              </div>
-              <div className='modal-body d-flex flex-column'>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-fullscreen-lg-down">
+          <div className="modal-content bg-black">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Agregar Imagen
+              </h1>
+            </div>
+            <div className="modal-body d-flex flex-column">
+              <form onSubmit={ModalHandler}>
                 {/* input file */}
-                <div className='input-group mb-3'>
+                <div className="input-group mb-3">
                   <input
                     onChange={(e) => {
                       setFile(e.target.files[0]);
                     }}
-                    type='file'
-                    className='form-control'
-                    id='inputGroupFile02'
+                    type="file"
+                    className="form-control"
+                    id="inputGroupFile02"
+                    required
                   />
                 </div>
                 {/* input text */}
-                <div className='input-group mb-3'>
+                <div className="input-group mb-3">
                   <input
+                    maxlength="43"
                     onChange={(e) => {
-                      setDescripcion(e.target.value);
+                      setDescripcion(e.target.value.toLocaleLowerCase());
                     }}
                     value={descripcion}
-                    className='form-control'
-                    placeholder='Descripcion'
+                    className="form-control"
+                    placeholder="Descripcion"
                     required
                   />
                 </div>
                 {/* select */}
-                <div className='input-group mb-3'>
-                  <button className='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                <div className="input-group mb-3">
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
                     Seleccione una categoria
                   </button>
 
-                  <ul className='dropdown-menu'>
+                  <ul className="dropdown-menu">
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Accion');
+                          setCategoria("Accion");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Accion
                       </a>
@@ -87,10 +99,10 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Amor');
+                          setCategoria("Amor");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Amor
                       </a>
@@ -98,10 +110,10 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Vintage');
+                          setCategoria("Vintage");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Vintage
                       </a>
@@ -109,10 +121,10 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Paisajes');
+                          setCategoria("Paisajes");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Paisajes
                       </a>
@@ -120,10 +132,10 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Minimalista');
+                          setCategoria("Minimalista");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Minimalista
                       </a>
@@ -131,10 +143,10 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('Dark');
+                          setCategoria("Dark");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Dark
                       </a>
@@ -142,42 +154,48 @@ function Modal() {
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('ligth');
+                          setCategoria("ligth");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         Ligth
                       </a>
                     </li>
                     <li>
-                      <hr className='dropdown-divider' />
+                      <hr className="dropdown-divider" />
                     </li>
                     <li>
                       <a
                         onClick={() => {
-                          setCategoria('all');
+                          setCategoria("all");
                         }}
-                        className='dropdown-item'
-                        href='#'
+                        className="dropdown-item"
+                        href="#"
                       >
                         all
                       </a>
                     </li>
                   </ul>
                 </div>
-              </div>
-              <div className='modal-footer'>
-                <button type='submit' className='btn bg-secondary'>
-                  Guardar
-                </button>
-                <button type='button' className='btn bg-secondary' data-bs-dismiss='modal'>
-                  Cerrar
-                </button>
-              </div>
+                <div className="input-group mb-3">
+                  <button type="submit" className="btn bg-secondary">
+                    Guardar Imagen
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn bg-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cerrar
+              </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
--- Frontend/src/components/noSession.jsx
+++ Frontend/src/components/noSession.jsx
@@ -1,7 +1,9 @@
+import "../styles/noSession.css"
+
 export function NoSession() {
   return (
-    <div className='container'>
-      <h2>No Tienes usa sesion Iniciada</h2>
+    <div className='container d-flex align-items-center justify-content-center'>
+      <h2>No Tienes una sesion Iniciada</h2>
     </div>
   );
 }
